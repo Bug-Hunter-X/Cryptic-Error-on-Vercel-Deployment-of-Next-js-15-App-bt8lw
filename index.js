ae135fabdfff8a02887e2fe7a740cb1b8f089bb0
BugHunter-X <askzenai@gmail.com>
2025-01-03 22:08:05 +0100

    Add index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+    </div>
+  );
+}
+```
+This simple Next.js app throws an error when deployed to Vercel.  The error message is cryptic and unhelpful.  The code itself seems correct.
